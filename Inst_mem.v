9e057d8187f63068ba46fc2cfa8b21ae05070b23
Zeyad Mustafa <zeyadmustafa2411@gmail.com>
2025-09-07 23:26:46 +0300

    I have edited the Inst_mem

--- Inst_mem.v
+++ Inst_mem.v
@@ -6,9 +6,9 @@ module instruction_memory (
     reg [31:0] Inst_mem [0:63]; // 64 x 32-bit instruction memory 
 
     always@(A) begin
-        $readmemh("program.mem", ROM);
+        $readmemh("program.mem", Inst_mem);
     end
 
-    assign RD = ROM[A[31:2]];  // Word-aligned access
+    assign RD = Inst_mem[A[31:2]];  // Word-aligned access
 
 endmodule
