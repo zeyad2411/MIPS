bf2648491ab971e6397d5c1543af75a0702b62a2
Zeyad Mustafa <zeyadmustafa2411@gmail.com>
2025-09-07 23:47:27 +0300

    I have edited the PC file

---
+++ Prog_counter.v
@@ -0,0 +1,19 @@
+module prog_counter(
+input clk,
+input areset,
+input load,
+input [31:0] PCNext,
+output [31:0] PC
+);
+
+always @(posedge clk or negedge areset) begin
+ if (!areset) begin
+    PC <= 32'b0;
+ end
+ else begin
+    if (load) begin
+        PC <= PCNext;
+    end
+ end
+end
+endmodule
