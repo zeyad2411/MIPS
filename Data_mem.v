dd435a01805490c2503537a68f51e69bcd4a7fd0
Zeyad Mustafa <zeyadmustafa2411@gmail.com>
2025-09-07 23:29:48 +0300

    I have edited the Data_mem

--- Data_mem.v
+++ Data_mem.v
@@ -3,7 +3,7 @@ input clk,
 input WE,
 input [31:0] A,
 input [31:0] WD,
-output [31:0] RD
+output reg [31:0] RD
 );
 reg [31:0] Data_mem [0:63]; // 64 x 32-bit Data memory
 always @(posedge clk) begin
